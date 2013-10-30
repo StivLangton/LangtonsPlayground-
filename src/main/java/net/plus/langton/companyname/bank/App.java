5bdeccc2fee4db2b0cf0440d3661415057ab7927
Steven langton <slangton@plus.net>
2013-10-30 18:25:18 +0000

    added additional text
    added additional text to my class

--- src/main/java/net/plus/langton/companyname/bank/App.java
+++ src/main/java/net/plus/langton/companyname/bank/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, This is my learning task. It will help me become better at my job, carrying out this tasks has helped me a lot,  i have learnt a lot working on this tasks. I now know how to find build issues on a job, create features, hot-fixes and have a full understanding of the release lifecycle. Amending text file using Vim editor" );
+        System.out.println( "Hello World, This is my learning task. It will 
+        	help me become better at my job, carrying out this tasks has helped me a lot,
+        	  i have learnt a lot working on this tasks. I now know how to find 
+        	  build issues on a job, create features, hot-fixes and have a full 
+        	understanding of the release lifecycle. Amending text file using Vim editor
+
+        	In a large software development project, where many developers work on the same code, keeping the repository stable is a challenge. Because of sheer number of commits that land on the repository, even if the chance of accidental regression per commit is small, they compound and make the repository unstable, blocking other developers.
+A traditional implementation of Jenkins, where it tests the tip of the repositoriy, doesn't help with this situation as regressions can be only discovered after they land on the tip of the repository.
+Therefore often the "solution" to the problem is to have every developer rigorously run tests before changes are pushed to the repository, thereby wasting precious human time on test executions, which ideally should be running on the server.
+The validated merge feature for Git in Jenkins Enterprise by CloudBees is a plugin that solves this problem by allowing developers to push commits (called "proposed changes") to Jenkins, specifying which branch it is intended for (called "upstream ref", for example "master".) Jenkins will then build the proposed changes, verify that it is of a good quality, and then push that change to the upstream repository.
+
+
+        	" );
     }
 }
