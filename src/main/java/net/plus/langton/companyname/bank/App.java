f8de47ad20012fafa2f55cec6cafc84930bfa87a
Steven langton <slangton@plus.net>
2013-10-30 18:12:39 +0000

    Added additional files
    made changes to the pom

---
+++ src/main/java/net/plus/langton/companyname/bank/App.java
@@ -0,0 +1,13 @@
+package net.plus.langton.companyname.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World, This is my learning task. It will help me become better at my job, carrying out this tasks has helped me a lot,  i have learnt a lot working on this tasks. I now know how to find build issues on a job, create features, hot-fixes and have a full understanding of the release lifecycle. Amending text file using Vim editor" );
+    }
+}
